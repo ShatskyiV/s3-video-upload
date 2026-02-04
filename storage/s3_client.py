8c53aeb43527f1c025f27db9dce70a1ea0278f21
Vitalii Shatskyi <vitalii.shatskyi@seagate.com>
2026-02-04 22:16:11 +0100

    format all

--- storage/s3_client.py
+++ storage/s3_client.py
@@ -1,18 +1,17 @@
 import boto3
-import logging
 
 # logging.basicConfig(level=logging.DEBUG)
 # boto3.set_stream_logger("botocore", level=logging.DEBUG)
 
-class S3Client:
 
+class S3Client:
     def __init__(self, **client_kwargs):
         self.client = None
         self.client_kwargs = client_kwargs
 
     def connect(self):
         if not self.client:
-            self.client = boto3.client('s3', **self.client_kwargs)
+            self.client = boto3.client("s3", **self.client_kwargs)
         return self.client
 
     def close(self):
